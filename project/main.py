87948201af464e6d685379180cdf326de0bde469
Robert St. Louis <stlouis9@gmail.com>
2024-05-09 09:26:18 -0700

    Add topic and flashcard models, post routes

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+from models import User, Flashcard, Topic
 
 db = SQLAlchemy()
 app = Flask(__name__, static_folder='templates/assets')
@@ -26,6 +27,24 @@ def signup():
 @app.route('/logout')
 def logout():
     return render_template('logout.html')
+
+@app.route('/add_flashcard', methods=['POST'])
+def add_flashcard():
+    question = request.form.get('question')
+    answer = request.form.get('answer')
+    topic = request.form.get('topic')
+    flashcard = Flashcard(question=question, answer=answer, topic=topic)
+    db.session.add(flashcard)
+    db.session.commit()
+    return redirect(url_for('profile'))
+
+@app.route('/add_topic', methods=['POST'])
+def add_topic():
+    topic = request.form.get('topic')
+    email = request.form.get('email')
+    topic = Topic(topic=topic, email=email)
+    db.session.add(topic)
+    db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
--- project/models.py
+++ project/models.py
@@ -7,3 +7,17 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(100))
+    email = db.Column(db.String(100), db.ForeignKey('user.email'))
+    user = db.relationship('User', back_populates='topics')
+
+class Flashcard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(1000))
+    answer = db.Column(db.String(1000))
+    topic = db.Column(db.String(100))
+    email = db.Column(db.String(100), db.ForeignKey('user.email'))
+    user = db.relationship('User', back_populates='flashcards')
