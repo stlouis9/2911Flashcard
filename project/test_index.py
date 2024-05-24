c690852dea6977bcaaced5410ba47dfe77da094f
raajvir7 <raajvirrandhawa2311@gmail.com>
2024-05-23 18:33:30 -0700

    adding additional tests

--- project/test_index.py
+++ project/test_index.py
@@ -37,4 +37,36 @@ def test_add_flashcards(test_client, login):
     assert response.status_code == 200  
     assert b"Hello" in response.data
 
+def test_signup_existing_email(test_client):
+    response = test_client.post('/signup', data={'email': 'existingemail@test', 'password': 'password', 'name': 'testuser'}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Email address already exists" in response.data
+
+def test_login_incorrect_credentials(test_client):
+    response = test_client.post('/login', data={'email': 'existingemail@test', 'password': 'wrongpassword'}, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Login" in response.data
+
+def test_filter_flashcards(test_client, login):
+    assert login.status_code == 200
+    response = test_client.post('/filter_flashcards', data={'topic': 'test'}, follow_redirects=True)
+    assert response.status_code == 200
+    
+
+# def test_delete_flashcard(test_client, login):
+#     assert login.status_code == 200
+#     response = test_client.post('/delete_flashcard', data={'flashcard_id': '41'}, follow_redirects=True)
+#     assert response.status_code == 200
+
+# def test_update_flashcard(test_client, login):
+#     assert login.status_code == 200
+#     response = test_client.post('/update_flashcard', data={'flashcard_id': '42', 'question': 'Updated Question', 'answer': 'Updated Answer', 'topic': 'Updated Topic'}, follow_redirects=True)
+#     assert response.status_code == 200
+#     assert b"Updated Question" in response.data
+
+
+
+
+
+
  
