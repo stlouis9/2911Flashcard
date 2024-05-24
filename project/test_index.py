be19d21122a0c21654167e46166bc9176575aeb5
Robert St. Louis <stlouis9@gmail.com>
2024-05-23 17:18:18 -0700

    test2

--- project/test_index.py
+++ project/test_index.py
@@ -16,9 +16,9 @@ def test_login(test_client):
 def test_logout(test_client, login):
     
     assert login.status_code == 200
-    logout_response = test_client.get('/logout', follow_redirects = True)
+    logout_response = test_client.get('/logout')
+    assert b"logged out" in logout_response.data
     assert logout_response.status_code == 200
-    assert b"Flash Wizard" in logout_response.data
 
 def test_signup(test_client):
     response = test_client.post('/signup', data={'email': 'testuser@test', 'password': 'password', 'name': 'testuser'}, follow_redirects = True)  # Make GET request to signup URL
